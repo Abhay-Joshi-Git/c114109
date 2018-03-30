643e1d6cba2d8d619689dc187fbf1073bde50e9f
Abhay Joshi <abhy.joshi@gmail.com>
2018-03-30 12:07:46 +0530

    added top navbar

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,34 +1,58 @@
 import React from 'react';
-import './header.css';
+import { Navbar, Nav, li, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => (
-  <header>
-    <div className="logo" />
-    <div className="top-nav">
-      <ul>
-        <li className="selected">Dashboard <i className="fa fa-angle-down" /></li>
-        <li>Customer <i className="fa fa-angle-down" />
-          <ul>
-            <li>Accounts</li>
-            <li>Contacts </li>
-            <li>Magento Customers</li>
-            <li>Business Customers</li>
-          </ul>
+  <Navbar className="main-header-navbar">
+    <Navbar.Header>
+      <Navbar.Brand>
+        <div className="logo" />
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <li role="presentation">
+          <Link to="/" role="button">Dashboard</Link>
         </li>
-        <li>Sales <i className="fa fa-angle-down" /></li>
-        <li>Activities <i className="fa fa-angle-down" /></li>
-        <li>Marketing <i className="fa fa-angle-down" /></li>
-        <li>Reports <i className="fa fa-angle-down" /> </li>
-        <li>Systems <i className="fa fa-angle-down" /></li>
-      </ul>
-    </div>
-    <div className="right-icons">
-      <div className="create-icon" />
-      <i className="fa fa-search" />
-      <i className="fa fa-bell" />
-      <span>John Deo <i className="fa fa-angle-down" /></span>
-    </div>
-  </header>
+        <NavDropdown eventKey={2} title="Customers" id="basic-nav-dropdown">
+          <MenuItem eventKey={2.1}><Link to="/accounts">Accounts</Link></MenuItem>
+          <MenuItem eventKey={2.2}><Link to="/contacts">Contacts</Link></MenuItem>
+          <MenuItem eventKey={2.3}><Link to="/magento-customers">Magento Customers</Link></MenuItem>
+          <MenuItem eventKey={2.4}><Link to="/business-customers">Business Customers</Link></MenuItem>
+        </NavDropdown>
+        <li role="presentation">
+          <Link to="/sales" role="button">Sales</Link>
+        </li>
+        <li role="presentation">
+          <Link to="/activities" role="button">Activities</Link>
+        </li>
+        <li role="presentation">
+          <Link to="/marketing" role="button">Marketing</Link>
+        </li>
+        <li role="presentation">
+          <Link to="/reports" role="button">Reports</Link>
+        </li>
+        <li role="presentation">
+          <Link to="/systems" role="button">Systems</Link>
+        </li>
+      </Nav>
+      <Nav pullRight>
+        <li role="presentation">
+          <div className="create-icon" />
+        </li>
+        <li role="presentation">
+          <i className="fa fa-search icon-btn" role="button" />
+        </li>
+        <li role="presentation">
+          <i className="fa fa-bell icon-btn" role="button" />
+        </li>
+        <li role="presentation">
+          <div className="user icon-btn" role="button">John Deo <i className="fa fa-angle-down" /></div>
+        </li>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
 
 export default Header;
