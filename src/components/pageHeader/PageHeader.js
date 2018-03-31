30be282537e82ee4a70ace369ef9fecb577eae90
Abhay Joshi <abhy.joshi@gmail.com>
2018-03-31 15:25:22 +0530

    page header: on data filter select

--- src/components/pageHeader/PageHeader.js
+++ src/components/pageHeader/PageHeader.js
@@ -1,22 +1,62 @@
 import React, { Fragment } from 'react';
 import { Row, Col, Dropdown, MenuItem } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import find from 'lodash/find';
+
 import PageBreadCrumbs from 'components/pageBreadCrumbs/PageBreadCrumbs';
 import './PageHeader.css';
 
-const PageHeader = (props) => {
-  const GetDataFilterDropDown = pageDataFilterOptions => (
+class PageHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDataFilterOption: this.getInitialiDataFilterOption(props),
+    };
+  }
+
+  onDataFilterSelect = (item) => {
+    this.setState({
+      selectedDataFilterOption: item.id,
+    });
+  }
+
+  getInitialiDataFilterOption(props) {
+    if (props.filterDropdown.defaultSeclectedOption) {
+      return props.filterDropdown.defaultSeclectedOption;
+    }
+    if (props.filterDropdown.filterOptions && props.filterDropdown.filterOptions.length) {
+      return props.filterDropdown.filterOptions[0].id;
+    }
+    return null;
+  }
+
+  getDataFilterNameById(filterId) {
+    const selectedFilter = find(
+      this.props.filterDropdown.filterOptions,
+      item => item.id === filterId,
+    );
+    return selectedFilter ? selectedFilter.name : '';
+  }
+
+  getDataFilterDropDown = pageDataFilterOptions => (
     <Fragment>
       {
         (pageDataFilterOptions) ?
           <Dropdown id="page-data-filter" className="page-filter-dropdown">
             <Dropdown.Toggle>
-              <h4 className="dis-i-b">{pageDataFilterOptions.caption}</h4>
+              <h4 className="dis-i-b">
+                {this.getDataFilterNameById(this.state.selectedDataFilterOption)}
+              </h4>
             </Dropdown.Toggle>
             <Dropdown.Menu>
               {
-                pageDataFilterOptions.filterOptions.map((item, index) => (
-                  <MenuItem key={item} active={index === 0}>{item}</MenuItem>
+                pageDataFilterOptions.filterOptions.map(item => (
+                  <MenuItem
+                    key={item.id}
+                    onSelect={() => this.onDataFilterSelect(item)}
+                  >
+                    {item.name}
+                  </MenuItem>
                 ))
               }
             </Dropdown.Menu>
@@ -25,7 +65,7 @@ const PageHeader = (props) => {
     </Fragment>
   );
 
-  const GetPageOptionsDropDown = () => (
+  getPageOptionsDropDown = () => (
     <Fragment>
       <Dropdown id="page-options" className="page-filter-dropdown options-filter pl-3">
         <Dropdown.Toggle>
@@ -38,23 +78,26 @@ const PageHeader = (props) => {
     </Fragment>
   );
 
-  return (
-    <Fragment>
-      <PageBreadCrumbs {...props} />
-      <Row className="page-header-options-container">
-        <Col sm={4}>
-          {GetDataFilterDropDown(props.filterDropdown)}
-          {GetPageOptionsDropDown()}
-        </Col>
-      </Row>
-    </Fragment>
-  );
-};
+  render() {
+    return (
+      <Fragment>
+        <PageBreadCrumbs {...this.props} />
+        <Row className="page-header-options-container">
+          <Col sm={4}>
+            {this.getDataFilterDropDown(this.props.filterDropdown)}
+            {this.getPageOptionsDropDown()}
+          </Col>
+        </Row>
+      </Fragment>
+    );
+  }
+}
 
 PageHeader.propTypes = {
   filterDropdown: PropTypes.shape({
     caption: PropTypes.string.isRequired,
-    filterOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
+    filterOptions: PropTypes.arrayOf(PropTypes.object).isRequired,
+    defaultSeclectedOption: PropTypes.string,
   }),
   pageCaption: PropTypes.string.isRequired,
 };
--- src/containers/customer/accounts/CustomerAccounts.js
+++ src/containers/customer/accounts/CustomerAccounts.js
@@ -2,12 +2,23 @@ import React, { Fragment } from 'react';
 // import { Row, Col } from 'react-bootstrap';
 import GridPage from 'components/gridPage/GridPage';
 
+const dataFilterOptions = [
+  {
+    id: 'all',
+    name: 'All Accounts',
+  },
+  {
+    id: 'duplicated',
+    name: 'Duplicated Accounts',
+  },
+];
+
 const CustomerAccounts = () => {
   const PageOptions = {
     headerOptions: {
       filterDropdown: {
         caption: 'Accounts Filter',
-        filterOptions: ['All Accounts', 'Duplicated Accounts'],
+        filterOptions: dataFilterOptions,
       },
       pageCaption: 'Customer / Accounts',
     },
